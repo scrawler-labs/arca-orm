11669fe0a4049ada6c4e3eafdfa7d7ca596de70f
Pranjal Pandey <pranjaltech10@gmail.com>
2022-12-13 14:34:40 +0530

    fix test

--- tests/Pest.php
+++ tests/Pest.php
@@ -4,15 +4,15 @@ use function Pest\Faker\faker;
 function db($uuid = "ID")
 {
     $connectionParams = array(
-        'dbname' => 'scrawtest',
-         'user' => 'root',
-         'password' => 'root1432',
-         'host' => '127.0.0.1',
-         'driver' => 'pdo_mysql',
+        'dbname' => 'test_database',
+        'user' => 'admin',
+        'password' => 'rootpass',
+        'host' => '127.0.0.1',
+        'driver' => 'pdo_mysql',
     );
-    
+
     $db = Scrawler\Arca\Facade\Database::connect($connectionParams);
-    
+
     if ($uuid == 'UUID') {
         $db->useUUID();
         return $db;
@@ -24,13 +24,13 @@ function db($uuid = "ID")
 
 function populateRandomUser($uuid = 'ID')
 {
-    for ($i=0;$i<5;$i++) {
+    for ($i = 0; $i < 5; $i++) {
         $user = db($uuid)->create('user');
         $user->name = faker()->name;
         $user->email = faker()->email;
         $user->dob = faker()->date;
         $user->age = faker()->randomNumber(2, false);
-        $user->active = $i%2==0 ? true : false;
+        $user->active = $i % 2 == 0 ? true : false;
         $user->address = faker()->streetAddress();
         $user->save();
     }
@@ -43,7 +43,7 @@ function createRandomUser($uuid = 'ID')
     $user->email = faker()->email;
     $user->dob = faker()->date;
     $user->age = faker()->randomNumber(2, false);
-    $user->active = faker()->randomNumber(2, false)%2==0 ? true : false;
+    $user->active = faker()->randomNumber(2, false) % 2 == 0 ? true : false;
     $user->address = faker()->streetAddress();
     $id = $user->save();
     return $id;
