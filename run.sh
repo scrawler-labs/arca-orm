ae4a5e885596580025cd650c4e405f1e1d64fd55
ipranjal <pranjaltech10@gmail.com>
2024-10-11 11:12:50 +0530

    chore: remove run.sh

--- run.sh
+++
@@ -1,5 +0,0 @@
-vendor/bin/rector process src/ --config vendor/thecodingmachine/safe/rector-migrate.php
-php-cs-fixer fix ./src --rules=@Symfony      
-php-cs-fixer fix ./test --rules=@Symfony  
-# XDEBUG_MODE=coverage ./vendor/bin/pest --coverage   
-# vendor/bin/phpstan analyse src --level 8      
